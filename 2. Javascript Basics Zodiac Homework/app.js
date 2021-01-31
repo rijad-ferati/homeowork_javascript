b3dd59600ef223ea1f06ceb477740c8716a88a92
Rijad <75546420+rijad-ferati@users.noreply.github.com>
2021-01-31 22:12:52 +0100

    Add files via upload

---
+++ 2. Javascript Basics Zodiac Homework/app.js
@@ -0,0 +1,28 @@
+// Basic Javascript Basics Homework 1, Chinese Zodiac Homework
+let userAge = prompt("Please enter your age of birth");
+
+if ((userAge - 4) % 12 === 0) {
+  alert("You are a Rat");
+} else if ((userAge - 4) % 12 === 1) {
+  alert("You are an Ox");
+} else if ((userAge - 4) % 12 === 2) {
+  alert("You are a Tiger");
+} else if ((userAge - 4) % 12 === 3) {
+  alert("You are a Rabbit ");
+} else if ((userAge - 4) % 12 === 4) {
+  alert("You are a Dragon");
+} else if ((userAge - 4) % 12 === 5) {
+  alert("You are a Snake");
+} else if ((userAge - 4) % 12 === 6) {
+  alert("You are a horse");
+} else if ((userAge - 4) % 12 === 7) {
+  alert("You are a Goat");
+} else if ((userAge - 4) % 12 === 8) {
+  alert("You are a Monkey");
+} else if ((userAge - 4) % 12 === 9) {
+  alert("You are a Rooster");
+} else if ((userAge - 4) % 12 === 10) {
+  alert("You are such a Dog!");
+} else if ((userAge - 4) % 12 === 11) {
+  alert("You are a pig");
+}
